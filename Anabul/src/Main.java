abb7b2c78e6e6c0617e9716449d0e46b5fb2fd5b
haikalimamridha <haikalimamridha@gmail.com>
2026-05-01 05:37:20 +0700

    fix pertemuan 7 folder

---
+++ Anabul/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        Anabul A1 = new Kucing();
+        A1.Gerak();
+        A1.Suara();
+
+        Anabul A2 = new Anjing();
+        A2.Gerak();
+        A2.Suara();
+
+        Anabul A3 = new Burung();
+        A3.Gerak();
+        A3.Suara();
+    }
+}
